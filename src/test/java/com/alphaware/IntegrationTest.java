12eaf8e3d3eb6ff13a7b6880f648aef1743de512
gunjalrushabh <rushabhgunjalrg17@gmail.com>
2023-09-26 22:53:11 +0530

    Initial version of customerDiginext generated by generator-jhipster@7.9.4

---
+++ src/test/java/com/alphaware/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.alphaware;
+
+import com.alphaware.CustomerDiginextApp;
+import com.alphaware.config.AsyncSyncConfiguration;
+import com.alphaware.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { CustomerDiginextApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
